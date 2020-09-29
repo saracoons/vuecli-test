ae6dd3fb5be396cb5fe5f7ffb9401acf76a4c957
Sara Coons <saracoons@mail.weber.edu>
2020-09-28 21:26:23 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h2> {{ title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+  return{
+      title: 'Hello World'
+  }
+}
+}
+
+</script>
+
+<style>
+h2 {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: pink;
+  margin-top: 60px;
+}
+</style>
